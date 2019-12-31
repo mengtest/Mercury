8c58e17c54413c90347ca700ae01f4dcb56f4604
ksgfk <37179748+ksgfk@users.noreply.github.com>
2019-12-31 19:24:01 +0800

    单例模式

---
+++ Assets/Scripts/Utility/MonoSingleton.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 可挂载单例
+/// </summary>
+public abstract class MonoSingleton<T> : MonoBehaviour where T : Component
+{
+	private static T _instance;
+
+	/// <summary>
+	/// 获取单例实例
+	/// </summary>
+	public static T Instance
+	{
+		get
+		{
+			if (_instance)
+			{
+				return _instance;
+			}
+
+			_instance = FindObjectOfType<T>();
+			if (_instance)
+			{
+				return _instance;
+			}
+
+			var obj = new GameObject { name = typeof(T).Name };
+			_instance = obj.AddComponent<T>();
+			return _instance;
+		}
+	}
+
+	protected virtual void Awake()
+	{
+		if (!_instance)
+		{
+			_instance = this as T;
+			DontDestroyOnLoad(gameObject);
+		}
+	}
+}
