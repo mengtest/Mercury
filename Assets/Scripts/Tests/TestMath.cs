ac065bd7a4d7e9120076062bf4bcfc305c06c15f
ksgfk <37179748+ksgfk@users.noreply.github.com>
2019-12-31 22:50:09 +0800

    数学工具

---
+++ Assets/Scripts/Tests/TestMath.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Tests
+{
+	public class TestMath
+	{
+		[Test]
+		public void TestMathSimplePasses()
+		{
+			//var res = MathUtility.AngleToRadian(45);
+			var a = MathUtility.GetRadianFromPosWithXAxis2D(new Vector3(0, 0), new Vector3(-1, -1));
+			Assert.AreEqual(MathUtility.RadianToAngle(a), 180+45);
+		}
+	}
+}
---
+++ Assets/Scripts/Utility/MathUtility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Unity.Mathematics;
+
+public static class MathUtility
+{
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static float RadianToAngle(float radian)
+	{
+		return radian * 180 / math.PI;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static float AngleToRadian(float angle)
+	{
+		return angle * math.PI / 180;
+	}
+
+	[Obsolete("未完成")]
+	public static float GetRadianFromPosWithXAxis2D(float3 pivot, float3 pos)
+	{
+		var relative = new float2(pivot.x, pivot.y) - new float2(pos.x, pos.y);
+		var alpha = relative.y / relative.x;
+		var r = math.atan(alpha);
+		return alpha < 0 ? math.PI + r : r;
+	}
+}
