19766f6a01d60cd5736a8fb7f864f1fff044b8c5
YumisLink <57590359+YumisLink@users.noreply.github.com>
2020-01-10 21:04:23 +0800

    update

--- Assets/Scripts/Entity/Property/MoveCapability.cs
+++ Assets/Scripts/Entity/Property/MoveCapability.cs
@@ -5,7 +5,8 @@ public class MoveCapability : IEntityProperty
 {
 	public bool doubleJumpReady;
 	public float moveSpeed = 2;
-	public float nowSpeed = 0;
+	public float nowSpeedX = 0;
+	public float nowSpeedY = 0;
 	public float maxSpeed = 15;
 	public float jumpSpeed = 0;
 	public float doubleJumpColdDownTime = 0;
--- Assets/Scripts/System/MoveSystem.cs
+++ Assets/Scripts/System/MoveSystem.cs
@@ -60,65 +60,78 @@ public class MoveSystem : IEntitySystem
 		}
 		*/
 
-		entity.transform.position += new Vector3(move.nowSpeed * Time.deltaTime, 0, 0);
+		entity.transform.position += new Vector3(move.nowSpeedX * Time.deltaTime, 0, 0);
+		entity.transform.position += new Vector3(0, move.nowSpeedY * Time.deltaTime, 0);
+		if (!state.isOnStep)
+		{
+			move.nowSpeedY = (move.nowSpeedY - 2 < -10) ? (-10) : (move.nowSpeedY - 2);
+		}
 		if (move.doubleJumpColdDownTime > 0) move.doubleJumpColdDownTime -= Time.deltaTime;
-		if (Input.GetKeyDown(KeyCode.W))
+		if (Input.GetKeyDown(KeyCode.Space))
 		{
 			if (state.isOnStep)
 			{
-				rigid.velocity = new Vector2(0, move.maxSpeed * 0.6f);
+				//rigid.velocity = new Vector2(0, move.maxSpeed * 0.55f);
+				move.nowSpeedY = 15;
 				move.doubleJumpReady = true;
 				move.doubleJumpColdDownTime = 0.2f;
 				state.isOnStep = false;
 			}
 			else if (move.doubleJumpColdDownTime <= 0 && move.doubleJumpReady)
 			{
-				rigid.velocity = new Vector2(0, move.maxSpeed * 0.45f);
+				//rigid.velocity = new Vector2(0, move.maxSpeed * 0.45f);
+				move.nowSpeedY = 15;
 				move.doubleJumpReady = false;
 			}
 		}
+		/*
 		if (Input.GetKeyDown(KeyCode.S))
 		{
 			if (state.isOnStep && state.standedStep.gameObject.GetComponent<Step>().canThrough)
 			{
 				Physics2D.IgnoreCollision(colli, state.standedStep.collider, true);
 			}
-		}
-		if (Input.GetKey(KeyCode.A))
+		}*/
+		if (Input.GetKey(KeyCode.A) & state.isOnStep)
 		{
-			move.nowSpeed -= 3 * move.moveSpeed;
-			if (move.nowSpeed < -2 * move.moveSpeed)
+			move.nowSpeedX -= 3 * move.moveSpeed;
+			if (move.nowSpeedX < -2 * move.moveSpeed)
 			{
-				move.nowSpeed = -2 * move.moveSpeed;
+				move.nowSpeedX = -2 * move.moveSpeed;
 			}
 			var raw = entity.transform.eulerAngles;
 			entity.transform.eulerAngles = new Vector3(raw.x, 0, raw.z);
 		}
-		else if (Input.GetKey(KeyCode.D))
+		else if (Input.GetKey(KeyCode.D) & state.isOnStep)
 		{
-			move.nowSpeed += 3 * move.moveSpeed;
-			if (move.nowSpeed > 2 * move.moveSpeed)
+			move.nowSpeedX += 3 * move.moveSpeed;
+			if (move.nowSpeedX > 2 * move.moveSpeed)
 			{
-				move.nowSpeed = 2 * move.moveSpeed;
+				move.nowSpeedX = 2 * move.moveSpeed;
 			}
 			var raw = entity.transform.eulerAngles;
 			entity.transform.eulerAngles = new Vector3(raw.x, 180, raw.z);
 		}
-		if (move.nowSpeed > 0)
+		if (move.nowSpeedX > 0 & state.isOnStep)
 		{
-			move.nowSpeed = move.nowSpeed - 1 < 0 ? 0 : move.nowSpeed - 1;
+			move.nowSpeedX = move.nowSpeedX - 1 < 0 ? 0 : move.nowSpeedX - 1;
 		}
 		else
 		{
-			move.nowSpeed = move.nowSpeed + 1 > 0 ? 0 : move.nowSpeed + 1;
+			move.nowSpeedX = move.nowSpeedX + 1 > 0 ? 0 : move.nowSpeedX + 1;
 		}
-		if (move.jumpSpeed > 0)
+
+		/*
+		if (move.jumpSpeed > 0 & state.isOnStep)
 		{
 			move.jumpSpeed = move.jumpSpeed - 1 < 0 ? 0 : move.jumpSpeed - 1;
 		}
+
+		
 		if (rigid.velocity.magnitude > move.maxSpeed)
 		{
 			rigid.velocity = new Vector2(0, -move.maxSpeed);
 		}
+		*/
 	}
 }
