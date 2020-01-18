3e10b71b506e27f907b14906608379cb34d4275c
ksgfk <37179748+ksgfk@users.noreply.github.com>
2020-01-18 14:06:26 +0800

    更新环境依赖

---
+++ Assets/Plugins/UltimateTextDamage/Scripts/ForceLayout.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Guirao.UltimateTextDamage
+{
+    public class ForceLayout : MonoBehaviour
+    {
+        void OnEnable( )
+        {
+            foreach( ContentSizeFitter contentSizeFitter in GetComponentsInChildren<ContentSizeFitter>( true ) )
+            {
+                contentSizeFitter.enabled = false;
+                contentSizeFitter.enabled = true;
+            }
+
+            LayoutRebuilder.ForceRebuildLayoutImmediate( transform as RectTransform );
+        }
+    }
+}
